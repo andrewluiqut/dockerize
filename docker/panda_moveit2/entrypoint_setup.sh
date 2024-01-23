d662c206d0f797eb4386b91bb4799bee69668c9d
Dasun Gunasinghe <dasun.gunasinghe@qut.edu.au>
2024-01-23 17:24:27 +1000

    New structure for compose, with the main docker-compose.yaml to be the entry point to defining a project specific config docker-compose.yaml -> define base robot type, ROS version and commander as well as volume mounts for project packages robot_arms/robot-arm-compose.yaml -> defines details around each base robot arm image (one level down from docker-compose.yaml) docker/base-compose.yaml -> defines the base packages that are built upon for use. Contains the main volume and environment definitions that are extended

--- docker/panda_moveit2/entrypoint_setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "> Configured for ROS $ROS_DISTRO"
-echo "> Sourcing workspace"
-source "$MOVEIT_WS/install/setup.bash"
-
-# Run CMD from Dockerfile or the overriding command from docker compose yaml file
-echo "> Running $@"
-exec "$@"
